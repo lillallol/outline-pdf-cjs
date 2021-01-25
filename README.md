46ad7bb4e362c1e484d499481127c77ed1366552
Ilias Spanoudakis <spanoudos@hotmail.com>
2021-01-25 20:13:59 +0200

    First commit to publish in npm.

---
+++ README.md
@@ -0,0 +1,44 @@
+## Table of contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Example](#example)
+- [License](#license)
+
+## Description
+
+Like [@lillallol/outline-pdf](https://www.npmjs.com/package/@lillallol/outline-pdf) but with a higher level API that takes advantage of the node file system.
+
+## Installation
+
+```bash
+npm install @lillallol/outline-pdf-cjs
+```
+
+## Example
+
+```ts
+import { outlinePdfCjs } from "@lillallol/outline-pdf";
+
+await outlinePdfCjs({
+	loadPath: "path/to/pdf/to/outline.pdf",
+	savePath: "path/to/save/outlined.pdf",
+	// first column  : page number
+	//                 negative for collapsing outline
+	// second column : outline depth
+	// third column  : outline title
+	outline: `
+         1||Title 1
+         2|-|Title 2
+        -3|--|Title 3
+         4|---|Title 4
+         5|---|Title 5
+         6|-|Title 6
+         7||Title 7
+    `,
+});
+```
+
+## License
+
+[MIT](https://github.com/lillallol/outline-pdf-cjs/blob/master/LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import * as fs from "fs";
+import * as pdfLib from "pdf-lib";
+
+import { outlinePdfFactory } from "@lillallol/outline-pdf";
+import { rejectIfPathExistsFactory } from "./rejectIfPathExistsFactory";
+import { throwIfPathDoesNotEndWithPdf } from "./throwIfPathDoesNotEndWithPdf";
+
+// export function outlinePdfCjsFactory(
+//     fs: {
+//         readFileSync: typeof _readFileSync;
+//         writeFileSync: typeof _writeFileSync;
+//         promises: {
+//             access: typeof _access;
+//         };
+//     },
+//     pdfLib: {
+//         PDFDocument: typeof _PDFDocument;
+//         PDFArray: typeof _PDFArray;
+//         PDFDict: typeof _PDFDict;
+//         PDFName: typeof _PDFName;
+//         PDFNull: typeof _PDFNull;
+//         PDFNumber: typeof _PDFNumber;
+//         PDFPageLeaf: typeof _PDFPageLeaf;
+//         PDFRef: typeof _PDFRef;
+//         PDFHexString: typeof _PDFHexString;
+//     }
+// ) {
+/**
+ * @description Adds outline to an outline-less pdf.
+ * @example
+ * await outlinePdf({
+ *     loadPath: "absolute/or/relative/path/to/pdf/to/outline.pdf",
+ *     savePath: "absolute/or/relative/path/to/save/outlined.pdf",
+ *     // first column  : page number
+ *     //                 negative for collapsing outline
+ *     // second column : outline depth
+ *     // third column  : outline title
+ *     outline: `
+ *           1||some title
+ *          12|-|some title
+ *         -30|--|some title
+ *          34|---|some title
+ *          35|---|some title
+ *          60|--|some title
+ *          67|-|some title
+ *          80||some title
+ *     `,
+ * });
+ */
+export /*  return  */ async function outlinePdfCjs(_: {
+	/**
+	 * @description Absolute or relative path to the outline-less pdf you want to add outline.
+	 */
+	loadPath: string;
+	/**
+	 * @description Absolute or relative path to where the newly created outlined pdf will be saved.
+	 */
+	savePath: string;
+	/**
+	 * @description A string representation of the outline.
+	 * @example
+	 * // first column  : page number
+	 * //                 negative for collapsing outline
+	 * // second column : outline depth
+	 * // third column  : outline title
+	 * `
+	 * 	  1||some title
+	 * 	 12|-|some title
+	 * 	-30|--|some title
+	 * 	 34|---|some title
+	 * 	 35|---|some title
+	 * 	 60|--|some title
+	 * 	 67|-|some title
+	 * 	 80||some title
+	 * `
+	 */
+	outline: string;
+}): Promise<void> {
+	const { loadPath, outline, savePath } = _;
+	const rejectIfPathExists = rejectIfPathExistsFactory(fs);
+	throwIfPathDoesNotEndWithPdf(loadPath);
+	await rejectIfPathExists(savePath);
+
+	const outlinePdf = outlinePdfFactory(pdfLib);
+
+	await outlinePdf.loadPdf(fs.readFileSync(loadPath));
+	outlinePdf.outline = outline;
+	outlinePdf.applyOutlineToPdf();
+	const pdf = await outlinePdf.savePdf();
+
+	fs.writeFileSync(savePath, pdf);
+}
+// }
